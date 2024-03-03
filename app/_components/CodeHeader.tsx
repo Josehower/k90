8d8f504bbb44e7534090b415be54603d2d8f098f
Jose Fernando Hower Barbosa <jose.hower@lingvano.com>
2024-03-03 19:03:38 +0100

    Update CodeHeader Props for generic name

--- app/_components/CodeHeader.tsx
+++ app/_components/CodeHeader.tsx
@@ -1,8 +1,8 @@
-interface CodeHeaderProps {
+interface Props {
   text: string
 }
 
-export default function CodeHeader({ text }: CodeHeaderProps) {
+export default function CodeHeader({ text }: Props) {
   return (
     <div className="rounded-t-md bg-zinc-200 px-4 py-2 font-mono text-sm text-neutral-700 dark:bg-zinc-700 dark:text-neutral-300">
       {text}
